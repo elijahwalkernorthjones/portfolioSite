a8d74a74cbdbaffc39900140e71824d5db49a8bb
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-03-09 16:28:32 -0500

    more stuff added to portfolio site

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+    lottie.loadAnimation({
+        container: document.getElementById('about-animation'), // the container element
+        path: '/Animation.json', // path to the .lottie file
+        renderer: 'svg', // renderer type (svg/canvas/html)
+        loop: true, // loop the animation
+        autoplay: true // autoplay the animation
+    });
+
